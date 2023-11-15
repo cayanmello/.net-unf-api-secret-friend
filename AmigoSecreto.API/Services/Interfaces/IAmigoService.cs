793142844682c55566e5a030f6faf5323b48dc30
Cayan Mello <cayanneves@gmail.com>
2023-11-14 23:39:41 -0300

    Implementando leitura e escrita do arquivo csv

--- AmigoSecreto.API/Services/Interfaces/IAmigoService.cs
+++ AmigoSecreto.API/Services/Interfaces/IAmigoService.cs
@@ -4,7 +4,7 @@ namespace AmigoSecreto.API.Services.Interfaces;
 
 public interface IAmigoService
 {
-    public Amigo Save(Amigo amigo);
+    public bool Save(Amigo amigo);
     public IEnumerable<Amigo> GetAll();
     public Amigo GetById(string id);
 }
