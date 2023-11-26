f98e30888bf1bff66296cdeb0d42960d7a256168
jvieira <contato.josevictorvieira@gmail.com>
2023-11-26 05:29:51 -0300

    Customizando paginas de login e registrar-se, do identity

---
+++ AmigoSecreto.Web/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AmigoSecreto.Web.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string RedeSocial { get; set; } = string.Empty;
+        public string RedeSocialUrlPath { get; set; } = string.Empty;
+    }
+}
--- AmigoSecreto.Web/Program.cs
+++ AmigoSecreto.Web/Program.cs
@@ -1,18 +1,11 @@
+using AmigoSecreto.Web.Data;
+using AmigoSecreto.Web.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using WebApplication1.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+ConfigureServices(builder);
 
 var app = builder.Build();
 
@@ -33,11 +26,31 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.MapRazorPages();
 
 app.Run();
+
+void ConfigureServices(WebApplicationBuilder builder) 
+{
+    var connectionString = 
+        builder.Configuration
+        .GetConnectionString("DefaultConnection") ?? 
+        throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(connectionString));
+
+    builder.Services.AddDefaultIdentity<ApplicationUser>(options =>  
+    {
+        options.SignIn.RequireConfirmedAccount = true;
+    }).AddEntityFrameworkStores<ApplicationDbContext>();
+
+    builder.Services.AddControllersWithViews();
+}
